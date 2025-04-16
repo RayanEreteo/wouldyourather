55c8b87d3187e796cde193cc6a6e0af1cb67182a
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-04-16 21:34:17 +0200

    feat: replace Navbar with DilemmaHome component and load data from JSON file

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,16 @@
-import Navbar from "./components/Navbar";
+import fs from "fs"
+import path from "path";
+import DilemmaHome from "./components/DilemmaHome";
 
 export default function Home() {
+  const filePath = path.join(process.cwd(), "/public/dummydata.json")
+  const fileData = fs.readFileSync(filePath, "utf8")
+  const data = JSON.parse(fileData)
+
+  console.log(data);
+  
+
   return (
-    <>
-      <nav>
-        <Navbar></Navbar>
-      </nav>
-      <div className="text-white">
-        <main>
-          <div className="red-choice flex justify-center items-center bg-red-500 w-[100vw] h-[50vh]">
-            <div className="title text-center">
-              <h1 className="question-title text-4xl w-[90vw]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. A dolor labore magnam consequuntur delectus! Aut, ipsa!</h1>
-            </div>
-          </div>
-          <div className="separator w-[100vw] h-1"></div>
-          <div className="blue-choice flex justify-center items-center bg-blue-700 w-[100vw] h-[50vh]">
-            <div className="title text-center">
-              <h1 className="question-title text-4xl w-[90vw]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores error ipsum quos, officia hic accusamus odio?</h1>
-            </div>
-          </div>
-        </main>
-      </div>
-    </>
+    <DilemmaHome baseData={data}/>
   );
 }
