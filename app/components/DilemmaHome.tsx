b5463924378742016bf2746c275418b75b8eaf74
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-04-16 21:35:08 +0200

    feat: add DilemmaHome component with question display and result handling

---
+++ app/components/DilemmaHome.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { FormEvent, useState } from "react"
+import Navbar from "./Navbar"
+
+function DilemmaHome({ baseData }: any) {
+    const [resultSended, setResultSended] = useState<boolean>(false)
+    const [questionData, setQuestionData] = useState<object>(baseData)
+
+    const conditionalHiding = {
+        poleResult: resultSended ? {display: "block"} : {display: "none"},
+        questionTitle: resultSended ? {display: "none"} : {display: "block"}
+    }    
+
+    function sendResult(e: FormEvent){
+        if (resultSended) return
+
+        e.preventDefault()
+        setResultSended(true)
+        console.log("sended !");
+    }
+
+    return (
+        <>
+            <nav>
+                <Navbar></Navbar>
+            </nav>
+            <div className="text-white">
+                <main>
+                    <div className="red-choice flex justify-center items-center bg-red-500 w-[100vw] h-[50vh] hover:cursor-pointer" onClick={sendResult}>
+                        <div className="choice-content text-center">
+                            <h1 className="question-title text-4xl w-[90vw]" style={conditionalHiding.questionTitle}>{baseData.redQuestion.title}</h1>
+                            <h1 className="question-pole-result text-9xl" style={conditionalHiding.poleResult}>{baseData.redQuestion.percentage + "%"}</h1>
+                        </div>
+                    </div>
+                    <div className="separator w-[100vw] h-1"></div>
+                    <div className="blue-choice flex justify-center items-center bg-blue-700 w-[100vw] h-[50vh] hover:cursor-pointer" onClick={sendResult}>
+                        <div className="title text-center">
+                            <h1 className="question-title text-4xl w-[90vw]" style={conditionalHiding.questionTitle}>{baseData.blueQuestion.title}</h1>
+                            <h1 className="question-pole-result text-9xl" style={conditionalHiding.poleResult}>{baseData.blueQuestion.percentage + "%"}</h1>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </>
+    )
+}
+
+export default DilemmaHome
