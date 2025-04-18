a6776eabcfd3551b417b51d2106c543350c822f8
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-04-18 15:07:09 +0200

    feat: update Navbar link text from "Add a question..." to "Add a dilemma..."

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 function Navbar() {
   return (
     <div id="navbar" className="absolute flex items-center w-[100vw] h-[7rem] text-white">
-        <a href="/addquestion" className="bg-blue-800 ml-7 text-center w-[200px] h-[50px] p-3 rounded transition-all hover:scale-120">Add a question...</a>
+        <a href="/addquestion" className="bg-blue-800 ml-7 text-center w-[200px] h-[50px] p-3 rounded transition-all hover:scale-120">Add a dilemma...</a>
     </div>
   )
 }
