af5a92fa5c0b54c134fd1fcc67de875f0b9c3168
RayanEreteo <rayabf5@gmail.com>
2025-04-24 17:58:51 +0200

    creating skeleton for "add dilemma" form

--- app/addquestion/page.tsx
+++ app/addquestion/page.tsx
@@ -2,8 +2,22 @@
 
 
 function Addquestion() {
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault(); // Prevent the default form submission behavior
+  }
+
   return (
-    <div>sdadas</div>
+    <main>
+      <h1>Add Dilemma</h1>
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="question" placeholder="Red" required />
+          <input type="text" name="answer" placeholder="Blue" required />
+          <button type="submit">Add Question</button>
+        </form>
+      </div>
+    </main>
   )
 }
 
