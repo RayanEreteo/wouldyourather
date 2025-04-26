ad0bb6e4bc32b67fdc1e7677a1077773633f3efb
RayanEreteo <rayabf5@gmail.com>
2025-04-26 16:31:45 +0200

    added BG gradient

--- app/addquestion/page.tsx
+++ app/addquestion/page.tsx
@@ -1,21 +1,22 @@
 "use client"
 
-
 function Addquestion() {
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault(); // Prevent the default form submission behavior
+    event.preventDefault();
   }
 
   return (
     <main>
-      <h1>Add Dilemma</h1>
-      <div className="form-container">
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="question" placeholder="Red" required />
-          <input type="text" name="answer" placeholder="Blue" required />
-          <button type="submit">Add Question</button>
-        </form>
+      <div className="addquestion flex flex-col justify-center items-center h-screen w-screen bg-gradient-to-b from-red-600 to-blue-700">
+        <h1 className="font-extrabold text-3xl mb-6">Add Dilemma</h1>
+        <div className="form-container">
+          <form onSubmit={handleSubmit}>
+            <input type="text" name="question" placeholder="Red" required />
+            <input type="text" name="answer" placeholder="Blue" required />
+            <button type="submit">Add Question</button>
+          </form>
+        </div>
       </div>
     </main>
   )
