5c644732f91999c154108cef32469169331a08f2
RayanEreteo <rayabf5@gmail.com>
2025-04-26 21:16:03 +0200

    feat: enhance Add Dilemma form with styling and structure improvements

--- app/addquestion/page.tsx
+++ app/addquestion/page.tsx
@@ -9,12 +9,13 @@ function Addquestion() {
   return (
     <main>
       <div className="addquestion flex flex-col justify-center items-center h-screen w-screen bg-gradient-to-b from-red-600 to-blue-700">
-        <h1 className="font-extrabold text-3xl mb-6">Add Dilemma</h1>
-        <div className="form-container">
-          <form onSubmit={handleSubmit}>
-            <input type="text" name="question" placeholder="Red" required />
-            <input type="text" name="answer" placeholder="Blue" required />
-            <button type="submit">Add Question</button>
+        <h1 className="text-white font-extrabold text-3xl mb-6">Add Dilemma</h1>
+        <div className="form-container bg-indigo-800 rounded p-3">
+          <form onSubmit={handleSubmit} className="flex flex-col">
+            <input className="mb-6 bg-white rounded p-4" type="text" name="question" placeholder="Red" required />
+            <h1 className="text-center text-white text-xl">OR</h1>
+            <input className="mt-6 mb-6 bg-white rounded p-4" type="text" name="answer" placeholder="Blue" required />
+            <button type="submit" className="text-white font-bold bg-cyan-500 rounded p-4 hover:cursor-pointer">Add Dilemma</button>
           </form>
         </div>
       </div>
