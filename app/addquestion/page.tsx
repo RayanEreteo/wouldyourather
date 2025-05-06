4dd7f90bd54baf754da60a5eb3d7a23cec4ea919
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-05-06 19:28:20 +0200

    feat: implement form handling with refs for Add Dilemma inputs

--- app/addquestion/page.tsx
+++ app/addquestion/page.tsx
@@ -1,9 +1,32 @@
 "use client"
 
+import { useRef } from "react";
+
 function Addquestion() {
 
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  const redRef = useRef<HTMLInputElement>(null);
+  const blueRef = useRef<HTMLInputElement>(null);
+
+ async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    const jsonData = {
+      red: redRef.current?.value,
+      blue: blueRef.current?.value
+    };
+
+    const res = await fetch("http://localhost:8000/addDilemma", 
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(jsonData)
+      }
+    )
+
+    const data = await res.json();
+    console.log(data);
   }
 
   return (
@@ -12,9 +35,9 @@ function Addquestion() {
         <h1 className="text-white font-extrabold text-3xl mb-6">Add Dilemma</h1>
         <div className="form-container bg-indigo-800 rounded p-3">
           <form onSubmit={handleSubmit} className="flex flex-col">
-            <input className="mb-6 bg-white rounded p-4" type="text" name="question" placeholder="Red" required />
+            <input ref={redRef} className="mb-6 bg-white rounded p-4" type="text" name="question" placeholder="Red" required />
             <h1 className="text-center text-white text-xl">OR</h1>
-            <input className="mt-6 mb-6 bg-white rounded p-4" type="text" name="answer" placeholder="Blue" required />
+            <input ref={blueRef} className="mt-6 mb-6 bg-white rounded p-4" type="text" name="answer" placeholder="Blue" required />
             <button type="submit" className="text-white font-bold bg-cyan-500 rounded p-4 hover:cursor-pointer">Add Dilemma</button>
           </form>
         </div>
