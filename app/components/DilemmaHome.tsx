d0bcffe4f5c3ac018739f04281f77a67a8f81620
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-05-13 14:03:31 +0200

    feat: update DilemmaHome component to use dynamic data from baseData

--- app/components/DilemmaHome.tsx
+++ app/components/DilemmaHome.tsx
@@ -5,7 +5,6 @@ import Navbar from "./Navbar"
 
 function DilemmaHome({ baseData }: any) {
     const [resultSended, setResultSended] = useState<boolean>(false)
-    const [questionData, setQuestionData] = useState<object>(baseData)
 
     const conditionalHiding = {
         poleResult: resultSended ? {display: "block"} : {display: "none"},
@@ -29,15 +28,15 @@ function DilemmaHome({ baseData }: any) {
                 <main>
                     <div className="red-choice flex justify-center items-center bg-red-500 w-[100vw] h-[50vh] hover:cursor-pointer" onClick={sendResult}>
                         <div className="choice-content text-center">
-                            <h1 className="question-title text-4xl w-[90vw]" style={conditionalHiding.questionTitle}>Test</h1>
-                            <h1 className="question-pole-result text-9xl" style={conditionalHiding.poleResult}>33</h1>
+                            <h1 className="question-title text-4xl w-[90vw]" style={conditionalHiding.questionTitle}>{baseData.returnedDilemma.red}</h1>
+                            <h1 className="question-pole-result text-9xl" style={conditionalHiding.poleResult}>{baseData.returnedDilemma.blue_click}</h1>
                         </div>
                     </div>
                     <div className="separator w-[100vw] h-1"></div>
                     <div className="blue-choice flex justify-center items-center bg-blue-700 w-[100vw] h-[50vh] hover:cursor-pointer" onClick={sendResult}>
                         <div className="title text-center">
-                            <h1 className="question-title text-4xl w-[90vw]" style={conditionalHiding.questionTitle}>Test</h1>
-                            <h1 className="question-pole-result text-9xl" style={conditionalHiding.poleResult}>33</h1>
+                            <h1 className="question-title text-4xl w-[90vw]" style={conditionalHiding.questionTitle}>{baseData.returnedDilemma.blue}</h1>
+                            <h1 className="question-pole-result text-9xl" style={conditionalHiding.poleResult}>{baseData.returnedDilemma.red_click}</h1>
                         </div>
                     </div>
                 </main>
