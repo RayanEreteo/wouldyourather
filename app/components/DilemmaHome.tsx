991f1b1662dab09c94ffc87d8a867d87c23414a8
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2025-05-15 17:57:50 +0200

    update locally clicks

--- app/components/DilemmaHome.tsx
+++ app/components/DilemmaHome.tsx
@@ -7,16 +7,30 @@ function DilemmaHome({ baseData }: any) {
     const [resultSended, setResultSended] = useState<boolean>(false)
 
     const conditionalHiding = {
-        poleResult: resultSended ? {display: "block"} : {display: "none"},
-        questionTitle: resultSended ? {display: "none"} : {display: "block"}
-    }    
+        poleResult: resultSended ? { display: "block" } : { display: "none" },
+        questionTitle: resultSended ? { display: "none" } : { display: "block" }
+    }
 
-    function sendResult(e: FormEvent){
+    async function sendResult(e: FormEvent, color: string) {
         if (resultSended) return
-
-        e.preventDefault()
         setResultSended(true)
-        console.log("sended !");
+
+        const data = {
+            id: baseData.returnedDilemma.id,
+            color: color
+        }
+
+        try {
+            await fetch("http://localhost:8000/setDilemmaClicks", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+        } catch (error) {
+
+        }
     }
 
     return (
@@ -26,17 +40,17 @@ function DilemmaHome({ baseData }: any) {
             </nav>
             <div className="text-white">
                 <main>
-                    <div className="red-choice flex justify-center items-center bg-red-500 w-[100vw] h-[50vh] hover:cursor-pointer" onClick={sendResult}>
+                    <div className="red-choice flex justify-center items-center bg-red-500 w-[100vw] h-[50vh] hover:cursor-pointer" onClick={(e) => sendResult(e, "red_click")}>
                         <div className="choice-content text-center">
                             <h1 className="question-title text-4xl w-[90vw]" style={conditionalHiding.questionTitle}>{baseData.returnedDilemma.red}</h1>
-                            <h1 className="question-pole-result text-9xl" style={conditionalHiding.poleResult}>{baseData.returnedDilemma.blue_click}</h1>
+                            <h1 className="question-pole-result text-9xl" style={conditionalHiding.poleResult}>{baseData.returnedDilemma.red_click + 1}</h1>
                         </div>
                     </div>
                     <div className="separator w-[100vw] h-1"></div>
-                    <div className="blue-choice flex justify-center items-center bg-blue-700 w-[100vw] h-[50vh] hover:cursor-pointer" onClick={sendResult}>
+                    <div className="blue-choice flex justify-center items-center bg-blue-700 w-[100vw] h-[50vh] hover:cursor-pointer" onClick={(e) => sendResult(e, "blue_click")}>
                         <div className="title text-center">
                             <h1 className="question-title text-4xl w-[90vw]" style={conditionalHiding.questionTitle}>{baseData.returnedDilemma.blue}</h1>
-                            <h1 className="question-pole-result text-9xl" style={conditionalHiding.poleResult}>{baseData.returnedDilemma.red_click}</h1>
+                            <h1 className="question-pole-result text-9xl" style={conditionalHiding.poleResult}>{baseData.returnedDilemma.blue_click + 1}</h1>
                         </div>
                     </div>
                 </main>
