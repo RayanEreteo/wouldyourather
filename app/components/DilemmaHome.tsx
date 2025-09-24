9d2cd5d484ee442ebd468f448b1f7e3dd64924ca
RayanEreteo <rayabf5@gmail.com>
2025-09-24 21:23:11 +0200

    Ajout TODO.

--- app/components/DilemmaHome.tsx
+++ app/components/DilemmaHome.tsx
@@ -1,5 +1,9 @@
 "use client"
 
+// TODO : - Mettre a jour la valeur coté client quand on envoie l’incrémentation au serveur.
+// TODO : - Mettre un chargement lors de l'envoi de la valeur.
+// TODO : - Redirection a une page erreur si serveur indisponible.
+
 import { FormEvent, useState } from "react"
 import Navbar from "./Navbar"
 
@@ -43,14 +47,14 @@ function DilemmaHome({ baseData }: any) {
                     <div className="red-choice flex justify-center items-center bg-red-500 w-[100vw] h-[50vh] hover:cursor-pointer" onClick={(e) => sendResult(e, "red_click")}>
                         <div className="choice-content text-center">
                             <h1 className="question-title text-4xl w-[90vw]" style={conditionalHiding.questionTitle}>{baseData.returnedDilemma.red}</h1>
-                            <h1 className="question-pole-result text-9xl" style={conditionalHiding.poleResult}>{baseData.returnedDilemma.red_click + 1}</h1>
+                            <h1 className="question-pole-result text-9xl" style={conditionalHiding.poleResult}>{baseData.returnedDilemma.red_click}</h1>
                         </div>
                     </div>
                     <div className="separator w-[100vw] h-1"></div>
                     <div className="blue-choice flex justify-center items-center bg-blue-700 w-[100vw] h-[50vh] hover:cursor-pointer" onClick={(e) => sendResult(e, "blue_click")}>
                         <div className="title text-center">
                             <h1 className="question-title text-4xl w-[90vw]" style={conditionalHiding.questionTitle}>{baseData.returnedDilemma.blue}</h1>
-                            <h1 className="question-pole-result text-9xl" style={conditionalHiding.poleResult}>{baseData.returnedDilemma.blue_click + 1}</h1>
+                            <h1 className="question-pole-result text-9xl" style={conditionalHiding.poleResult}>{baseData.returnedDilemma.blue_click}</h1>
                         </div>
                     </div>
                 </main>
